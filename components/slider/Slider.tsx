9922d5e5a31c713a60c138654894e548a13b5950
Tomás Keong <tomaskeong@gmail.com>
2021-09-25 15:17:06 +0100

    Added animations from aos library

--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -22,6 +22,16 @@ const Slider = () => {
           <div className="hb-info">
             <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a>
           </div>
+          <div className="hb-lang">
+            <ul className="nav">
+              <li className="active">
+                <a href="#">EN</a>
+              </li>
+              <li>
+                <a href="#">FR</a>
+              </li>
+            </ul>
+          </div>
         </div>
         {/* End hp-top-fixed */}
 
@@ -29,19 +39,47 @@ const Slider = () => {
           <div className="row full-screen align-items-center">
             <div className="col-lg-7">
               <div className="type-box">
-                <h6>Hi, my name is</h6>
-                <h1 className="font-alt">{sliderContent.name}</h1>
-                <TextLoop>
-                  <p className="loop-text lead"> Lead Engineer</p>
-                  <p className="loop-text lead"> Technical Lead</p>
-                  <p className="loop-text lead"> Web Developer</p>
-                  <p className="loop-text lead"> Music Enthusiast</p>
-                </TextLoop>
-                <p className="desc">{sliderContent.description}</p>
-                <div className="mt-4">
+                <h6 data-aos="fade-up" data-aos-duration="1200">
+                  Hello, My name is
+                </h6>
+                <h1
+                  className="font-alt"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="100"
+                >
+                  {sliderContent.name}
+                </h1>
+                <div
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="200"
+                >
+                  <TextLoop>
+                    <p className="loop-text lead"> Lead Engineer</p>
+                    <p className="loop-text lead"> Technical Lead</p>
+                    <p className="loop-text lead"> Web Developer</p>
+                    <p className="loop-text lead"> Music Enthusiast</p>
+                  </TextLoop>
+                </div>
+
+                <p
+                  className="desc"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="300"
+                >
+                  {sliderContent.description}
+                </p>
+                <div
+                  className="mt-4"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="400"
+                >
                   <a
                     className="px-btn px-btn-white"
-                    href="doc/CV_Tomas_Keong.pdf"
+                    href="docs/CV_Tomas_Keong.pdf"
                     download
                   >
                     {sliderContent.btnText}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app"
+import { useEffect } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
 import "./../assets/styles/main.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init()
+  }, [])
   return <Component {...pageProps} />
 }
 export default MyApp
