295b8646c004cbf43dba5b6b3c9f0690f3cce0df
Tomás Keong <tomaskeong@gmail.com>
2021-09-26 01:20:08 +0100

    Corrected imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,7 @@ import { useEffect } from "react"
 import AOS from "aos"
 import "aos/dist/aos.css"
 import dynamic from "next/dynamic"
-import ScrollToTop from "../components/ScrollToTop"
-import "assets/styles/main.scss"
+import "@styles/main.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -12,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [])
 
   const DynamicAnimatedCursor = dynamic(
-    () => import("./../components/animated-cursor/AnimatedCursorComponent"),
+    () => import("components/animated-cursor/AnimatedCursorComponent"),
     {
       ssr: false,
     }
