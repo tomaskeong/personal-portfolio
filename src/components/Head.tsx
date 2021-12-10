ea84bdb362ef5656007f8724254827684e2e30fe
Tomás Keong <tomaskeong@gmail.com>
2021-12-10 20:57:13 +0000

    added certifications text to content SEO

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -12,7 +12,7 @@ const HeadComponent = ({
       <title>{title}</title>
       <meta
         name="description"
-        content="Website containing Tomás Keong's information such as professional experience, education, skills and resume."
+        content="Website containing Tomás Keong's information such as professional experience, education, skills, resume and certifications."
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
